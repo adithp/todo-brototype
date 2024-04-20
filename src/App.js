8c53076214c5be758d680c941f2423ecc40c4459
adithp <adithkandoth@gmail.com>
2024-04-20 17:51:11 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [todos,setTodos] = useState([])
+    const [todo,setTodo] = useState('')
+	
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -12,19 +16,49 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={todo} onChange={(e)=> setTodo(e.target.value)}  type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=> {
+			setTodos([...todos,{id:Date.now(),text:todo,status:false}])
+			setTodo('')
+			}} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        {
+			todos.map((item,index) => {
+				return (
+				<div className="todo" key={index} >
+					<div className="left">
+						<input value={item.status} onChange={(e)=> {
+							console.log(todos)
+							setTodos(todos.filter(item2 => {
+								if(item2.id === item.id){
+									item2.status=e.target.checked
+								}
+								return item2
+								
+							}))
+						}} type="checkbox" name="" id="" />
+						<p>{item.text}</p>
+					</div>
+					<div className="right">
+						<i className="fas fa-times" 
+						onClick={()=> {
+							setTodos(todos.filter(item2 => {
+								if(item2.id === item.id) {
+									item2 = null
+								}
+								return item2
+							}))
+						}}
+						></i>
+					</div>
+		  		</div>
+				)
+			})
+			
+		  
+		  }
+       
       </div>
     </div>
   );
